72354afb545d2b957865c1f86db226aaa85a10fa
Mariia Matveieva <matveieva.mariya@gmail.com>
2019-03-13 07:50:09 +0200

    Initial commit

---
+++ CodeFirstDataAccess/BookEntityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstModel;
+using System.Data.Entity;
+
+namespace CodeFirstDataAccess
+{
+    public class BookEntityContext : DbContext
+    {
+        public BookEntityContext()
+        : base("name=DbConnectionString")
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CodeFirstWebApp/Models/BookModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeFirstDataAccess;
+using CodeFirstModel;
+using CodeFirstWebApp.Models;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace CodeFirstWebApp.Models
+{
+    public class BookModel
+    {
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Edition { get; set; }
+        public string Publishing { get; set; }
+    }
+}
